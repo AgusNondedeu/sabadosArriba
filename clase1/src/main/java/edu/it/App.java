ec17d165246a3ed62da20d3d246edd64a48a6cf4
andy <arteysoft@gmail.com>
2022-07-30 11:35:13 -0300

    ejercicio 1

--- clase1/src/main/java/edu/it/App.java
+++ clase1/src/main/java/edu/it/App.java
@@ -1,14 +1,11 @@
 package edu.it;
 
-import edu.it.factories.Factory;
-import edu.it.repository.GrabadorCliente;
-import edu.it.repository.GrabadorClienteJSON;
-import edu.it.repository.LectorCliente;
-import edu.it.repository.LectorClienteStdIn;
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Curso patrones - Bienvenidos");
+        
+        
+        
         );
     }
 }
