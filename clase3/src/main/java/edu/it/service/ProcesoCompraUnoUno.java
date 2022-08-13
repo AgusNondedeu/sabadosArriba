5a269003d591da9b44f0bd18b468d1d5c801ffee
andy <arteysoft@gmail.com>
2022-08-13 10:30:41 -0300

    agrego inyeccion de dependencia

--- clase3/src/main/java/edu/it/service/ProcesoCompraUnoUno.java
+++ clase3/src/main/java/edu/it/service/ProcesoCompraUnoUno.java
@@ -6,6 +6,11 @@ public class ProcesoCompraUnoUno implements ProcesoCompra {
 	private LectorDeCompra lectorDeCompra;
 	private GrabadorDeCompra grabadorDeCompra;
 	
+	public ProcesoCompraUnoUno(LectorDeCompra lectorDeCompra, GrabadorDeCompra grabadorDeCompra) {
+		this.lectorDeCompra = lectorDeCompra;
+		this.grabadorDeCompra = grabadorDeCompra;
+	}
+	
 	public void run() {
 		var compra = lectorDeCompra.leer();
 		grabadorDeCompra.grabar(compra);
