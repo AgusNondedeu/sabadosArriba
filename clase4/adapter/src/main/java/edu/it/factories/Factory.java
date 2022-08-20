beb5892c725c4451152f8a026c8ade605f036c37
andy <arteysoft@gmail.com>
2022-08-20 11:22:43 -0300

    probar

--- clase4/adapter/src/main/java/edu/it/factories/Factory.java
+++ clase4/adapter/src/main/java/edu/it/factories/Factory.java
@@ -1,10 +1,12 @@
 package edu.it.factories;
 
-import edu.it.services.DiscadorPotencia;
+import dialerpluspro.DialerPro;
+import edu.it.services.DiscadorAdapter;
 import edu.it.services.ServicioDiscador;
 
 public class Factory {
 	public static ServicioDiscador crearDiscador() {
-		return new ServicioDiscador(new DiscadorPotencia());
+		// return new ServicioDiscador(new DiscadorPotencia());
+		return new ServicioDiscador(new DiscadorAdapter(new DialerPro()));
 	}
 }
